16b888fb7c904d3a7078e49610ddf4241c612fbc
CrisGamboa99 <cristian.gamboa.99@outlook.com>
2024-11-29 11:08:18 -0600

    Se comenta y corrigen los errores de los archivos html y JavaScript

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,30 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+
+/* Se arreglan las etiquetas que se estan llamando. Las etiquetas tienen clases, por lo tanto tienen que empezar con punto y no # */
+const $n = document.querySelector('.name');
+const $b = document.querySelector('.blog');
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+/* Para utilizar la expresión await se necesita que la función sea asíncrona, por lo tanto se añade async */
+async function displayUser(username) {
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
+  /* La variable data no esta definida, por lo tanto se crea como una constante.
+  Lo que se le asigna a la variable es para que se cree un json de la respuesta de la api. */
+  const data = await response.json();
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  /* Se cambian las '' por ``, para la sintaxis correcta */
+  $n.textContent = `${data.name}`;
+  $b.textContent = `${data.blog}`;
+  $l.textContent = `${data.location}`;
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  /* Se añade el $ a n para definirla correctamente */
+  $n.textContent = `Algo salió mal: ${err}`
 }
 
 displayUser('stolinski').catch(handleError);
